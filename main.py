2fa7833e5667cb45906eaedbb685d0fefda37f4b
happywgl <wyswys2010@gmail.com>
2016-04-08 13:25:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb    
 from lessons.dom_injection import DomInjection, DomInjectionCheck
 from lessons.sql_injection_1 import SqlInjection1
+from lessons.concurrency import *
 import json
 import webapp2
 
@@ -14,13 +15,19 @@ import logging
         
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello!<br>')
-        self.response.write('Start your <a href="/edit/form">form</a>')        
+        self.response.write('Hello!<br><br>')
+        self.response.write('Lessons list:<br><br>')
+        self.response.write('&nbsp;&nbsp;&nbsp;&nbsp;<a style="font-size:28;" href="/dom_injection">dom_injection</a><br>')
+        self.response.write('&nbsp;&nbsp;&nbsp;&nbsp;<a style="font-size:28;" href="/concurrency">concurrency</a><br>')
         
 app = webapp2.WSGIApplication([    
     ('/', MainHandler),  
     ('/dom_injection',DomInjection),
     ('/sql_injection_1',SqlInjection1),
     ('/dom_injection/check',DomInjectionCheck),
+    ('/concurrency',Concurrency),
+    ('/concurrency/check',ConcurrencyCheck),
+    ('/concurrency/lock',ConcurrencyLock),
+    ('/concurrency/buy',ConcurrencyBuy),
     ('*.', MainHandler)
     ], debug=True)
